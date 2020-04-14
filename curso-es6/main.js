4bcac09f4d348a8e147887a5806f4d4f81802fc1
ElizeuAlves <elizeu151@outlook.com>
2020-04-13 23:36:53 -0300

    Conceitos Iniciais

---
+++ curso-es6/main.js
@@ -0,0 +1,51 @@
+/*class List{
+    constructor(){
+        this.data = [];
+    }
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data)
+    }
+}
+
+class TodoList extends List{
+    constructor(){
+        super();
+
+        this.usuario = 'Teste';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
+
+class Matematica{
+
+    static soma(a, b){
+        return a + b;
+    }
+}
+
+console.log(Matematica.soma(2, 5))*/
+
+const a = 1; // Usando uma const não é possível reatribuir a variável;
+
+function teste(x){
+    let y = 2; // Let é a keyword para trabalhar com variáveis de escopo
+
+    if (x > 5){
+        console.log(x, y)
+    }
+}
+
+teste(10)
